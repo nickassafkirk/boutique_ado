1830d7c9634df2622ebf869c5754b112057adac4
nickassafkirk <nickassafkirk@gmail.com>
2021-07-14 22:20:50 +0000

    Added bag contect and delivery logic

---
+++ bag/contexts.py
@@ -0,0 +1,36 @@
+from decimal import Decimal
+from django.conf import settings
+
+
+def bag_contents(request):
+    """ 
+    this is known as a contect processor. Instead of returning a template it returns the context object
+    that we create and makes it available to all templates across the entire application.
+    It's similar top how you can use request.user in any template.
+    we need to add it to the context_processors list in the settings.py file 
+    """
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
