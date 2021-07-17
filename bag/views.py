1d2abef395cf97a499c2ada4eb5d5e8aba392308
nickassafkirk <nickassafkirk@gmail.com>
2021-07-17 14:42:26 +0000

    completed product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,16 +12,27 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})  
-    # checks if 'bag' object exists in session cookies and creates an empty object if it doesn't
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
-    # checks if item_id is in the bag already. If it is, it increases quantity
-    # if not it's added to the bag dictionary
+        # checks if 'bag' object exists in session cookies and creates an empty object if it doesn't
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+        # checks if item_id is in the bag already. If it is, it increases quantity
+        # if not it's added to the bag dictionary
 
     request.session['bag'] = bag
     return redirect(redirect_url)
- 
