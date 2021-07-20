af678c41099751dbd15883dfbef865874243dd9a
nickassafkirk <nickassafkirk@gmail.com>
2021-07-20 11:55:17 +0000

    Add checkout form and models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+# Creates orderline item admin inline to be used within the main order admin panel
+# We inherit the admin.TabularInline class
+class OrderLineItemAdminInlne(admin.TabularInline):
+    # define the model we want to access in admin
+    model = OrderLineItem
+    # set read_only fields
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    # Allows us to use Orderline item within order admin panel
+    inlines = (OrderLineItemAdminInlne,)
+
+    # List fields that are read_only to prevent accidental changes
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    # This defines the order of the displayed fields. Otherwise this
+    # would be done automatically by django using default order of fields
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country', 
+              'postcode', 'town_or_city', 'street_address1', 
+              'street_address2', 'county', 'delivery_cost', 
+              'order_total', 'grand_total',)
+
+    # This restricts the fields that appear in the order list to a few key items
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    # Here we order by date newest to oldest
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 
+                  'street_address1', 'street_address2', 
+                  'town_or_city', 'postcode', 'country', 
+                  'county',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto generated
+        labels and set autofocus on first field
+        """
+
+        # We use placeholders to overide default labels for form inputs
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': "Full Name",
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2' 'Street Address 2',
+            'county': 'County',
+        }
+
+        # set autofocus on full name field
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *' # puts a star on the placeholder if it's required
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder # applies placholder from varable above to each forms placeholder attr
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input' # adds a css class
+            self.fields[field].label = False # removes the forms input label
