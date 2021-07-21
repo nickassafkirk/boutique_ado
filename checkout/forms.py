f984e00629e0b905e42fb993e4171fce3daab767
nickassafkirk <nickassafkirk@gmail.com>
2021-07-21 23:06:14 +0000

    Updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,21 +22,21 @@ class OrderForm(forms.ModelForm):
             'full_name': "Full Name",
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, state or Locality',
         }
 
         # set autofocus on full name field
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'  # puts a star on the placeholder if it's required
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder  # applies placholder from varable above to each forms placeholder attr
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'  # puts a star on the placeholder if it's required
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder  # applies placholder from varable above to each forms placeholder attr
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'  # adds a css class
             self.fields[field].label = False  # removes the forms input label
